67b1aa859fee076f7c7196bf3a4ed28d5fe2209d
Dipak Poudel <pdl.dipak@gmail.com>
2021-07-06 09:59:24 +0200

    validator function

---
+++ src/utils/utils.js
@@ -0,0 +1,19 @@
+export const emailValidator = (email) => {
+  const re = /\S+@\S+\.\S+/;
+  if (!email || email.length <= 0) return 'Email cannot be empty.';
+  if (!re.test(email)) return 'Need a valid email address.';
+
+  return '';
+};
+
+export const passwordValidator = (password) => {
+  if (!password || password.length <= 0) return 'Password cannot be empty.';
+
+  return '';
+};
+
+export const nameValidator = (name) => {
+  if (!name || name.length <= 0) return 'Name cannot be empty.';
+
+  return '';
+};
